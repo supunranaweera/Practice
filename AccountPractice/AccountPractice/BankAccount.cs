40e71884e5fcb271565fa4f3584a640c14b3e26d
supunranaweera <57324451+supunranaweera@users.noreply.github.com>
2025-03-12 17:18:22 -0600

    creating accounts

---
+++ AccountPractice/AccountPractice/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountPractice
+{
+    internal class BankAccount
+    {
+
+
+            private float _balance;
+            private string _accountHolderName;
+            private int _accountNumber;
+            private string _emailAddress;
+            private string _phoneNumber;
+            private string _postalCode;
+            private string _password;
+
+
+            public BankAccount(float balance, string accountHolderName, int accountNumber, string emailAddress, string phoneNumber, string postalCode, string password)
+            {
+                _balance = balance;
+                _accountHolderName = accountHolderName;
+                _accountNumber = accountNumber;
+                _emailAddress = emailAddress;
+                _phoneNumber = phoneNumber;
+                _postalCode = postalCode;
+                _password = password;
+            }
+    }
+}
--- AccountPractice/AccountPractice/Program.cs
+++ AccountPractice/AccountPractice/Program.cs
@@ -1,34 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AccountPractice;
 using System.Security;
 
-Console.WriteLine("Hello, World!");
 
 
-public class BankAccount
-{
-
-
-    private float _balance;
-    private string _accountHolderName;
-    private int _accountNumber;
-    private string _emailAddress;
-    private string _phoneNumber;
-    private string _postalCode;
-    private string _password;
-
-
-    public BankAccount(float balance, string accountHolderName, int accountNumber, string emailAddress, string phoneNumber, string postalCode, string password)
-    {
-        _balance = balance;
-        _accountHolderName = accountHolderName;
-        _accountNumber = accountNumber;
-        _emailAddress = emailAddress;
-        _phoneNumber = phoneNumber;
-        _postalCode = postalCode;
-        _password = password;
-    }
-
-
-    BankAccount SupunAccount = new BankAccount(0, "Supun", 1, "supun@ranaweera.ca", "587-340-8398", "T6W 0M8", "1111");
-    BankAccount KamalAccount = new BankAccount(0, "Kamal", 2, "kamal@ranaweera.ca", "780-965-2012", "T6W 0M8", "1111");
-}
+BankAccount SupunAccount = new BankAccount(0, "Supun", 1, "supun@ranaweera.ca", "587-340-8398", "T6W 0M8", "1111");
+BankAccount KamalAccount = new BankAccount(0, "Kamal", 2, "kamal@ranaweera.ca", "780-965-2012", "T6W 0M8", "1111");
