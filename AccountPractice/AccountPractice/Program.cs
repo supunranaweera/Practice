e119bb512765207105ac19b09c4bddaffd76421e
supunranaweera <57324451+supunranaweera@users.noreply.github.com>
2025-03-12 00:24:28 -0600

    Update Program.cs

--- AccountPractice/AccountPractice/Program.cs
+++ AccountPractice/AccountPractice/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security;
+
 Console.WriteLine("Hello, World!");
 
 
@@ -25,4 +27,8 @@ public class BankAccount
         _postalCode = postalCode;
         _password = password;
     }
+
+
+    BankAccount SupunAccount = new BankAccount(0, "Supun", 1, "supun@ranaweera.ca", "587-340-8398", "T6W 0M8", "1111");
+    BankAccount KamalAccount = new BankAccount(0, "Kamal", 2, "kamal@ranaweera.ca", "780-965-2012", "T6W 0M8", "1111");
 }
