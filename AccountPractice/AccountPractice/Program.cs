36bd946511013e4798f0400d63f26ae24e20d7fd
supunranaweera <57324451+supunranaweera@users.noreply.github.com>
2025-03-12 00:19:53 -0600

    Update Program.cs

--- AccountPractice/AccountPractice/Program.cs
+++ AccountPractice/AccountPractice/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+public class BankAccount
+{
+
+
+    private float _balance;
+    private string _accountHolderName;
+    private int _accountNumber;
+    private string _emailAddress;
+    private string _phoneNumber;
+    private string _postalCode;
+    private string _password;
+
+
+    public BankAccount(float balance, string accountHolderName, int accountNumber, string emailAddress, string phoneNumber, string postalCode, string password)
+    {
+        _balance = balance;
+        _accountHolderName = accountHolderName;
+        _accountNumber = accountNumber;
+        _emailAddress = emailAddress;
+        _phoneNumber = phoneNumber;
+        _postalCode = postalCode;
+        _password = password;
+    }
+}
