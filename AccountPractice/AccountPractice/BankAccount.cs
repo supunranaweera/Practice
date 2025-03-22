c3dce40f62943dee52c8c302d2bf8535b3c77c1c
supunranaweera <57324451+supunranaweera@users.noreply.github.com>
2025-03-21 20:15:39 -0600

    created bank account classes

--- AccountPractice/AccountPractice/BankAccount.cs
+++ AccountPractice/AccountPractice/BankAccount.cs
@@ -4,30 +4,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AccountPractice
 {
     internal class BankAccount
     {
 
 
-            private float _balance;
-            private string _accountHolderName;
-            private int _accountNumber;
-            private string _emailAddress;
-            private string _phoneNumber;
-            private string _postalCode;
-            private string _password;
+        private float _balance;
+        private string _accountHolderName;
+        private int _accountNumber;
+        private string _emailAddress;
+        private string _phoneNumber;
+        private string _postalCode;
+        private string _password;
 
 
-            public BankAccount(float balance, string accountHolderName, int accountNumber, string emailAddress, string phoneNumber, string postalCode, string password)
+        public BankAccount(float balance, string accountHolderName, int accountNumber, string emailAddress, string phoneNumber, string postalCode, string password)
+        {
+            _balance = balance;
+            _accountHolderName = accountHolderName;
+            _accountNumber = accountNumber;
+            _emailAddress = emailAddress;
+            _phoneNumber = phoneNumber;
+            _postalCode = postalCode;
+            _password = password;
+        }
+
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Name: {_accountHolderName}");
+            Console.WriteLine($"Balance {_balance}");
+            DateTime.UtcNow.ToString() 
+        }
+
+        public void Deposit(float input)
+        {
+            balance = balance + input;
+        }
+
+        public void Withdraw(float input)
+        {
+            balance = balance - input;
+        }
+        public void Prompt()
+        {
+            while (true)
             {
-                _balance = balance;
-                _accountHolderName = accountHolderName;
-                _accountNumber = accountNumber;
-                _emailAddress = emailAddress;
-                _phoneNumber = phoneNumber;
-                _postalCode = postalCode;
-                _password = password;
+                Console.WriteLine("Input the selection");
+                Console.WriteLine("D - Deposit");
+                Console.WriteLine("W - Withdraw");
+                Console.WriteLine("P - Print balance");
+                Console.WriteLine("X - Exit");
+                Console.WriteLine("Your choice: ");
+                string choice = Console.ReadKey();
+
+
+                if (choice == "d")
+                {
+                    Conosle.WriteLine("Amount: ");
+                    float amount = float.Parse(Console.ReadLine());
+                    Deposit(amount);
+                    PrintBalance()
+                }
+
+
+                else if (choice == "w")
+                {
+                    float amount = Console.ReadLine();
+                    Withdraw(amount);
+                    PrintBalance();
+                }
+
+
+                else if (choice == "p")
+                {
+                    PrintBalance();
+                }
+
+                else if (choice == "x")
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+
             }
+
+        }
+
+
     }
 }
--- AccountPractice/AccountPractice/Program.cs
+++ AccountPractice/AccountPractice/Program.cs
@@ -6,3 +6,11 @@ using System.Security;
 
 BankAccount SupunAccount = new BankAccount(0, "Supun", 1, "supun@ranaweera.ca", "587-340-8398", "T6W 0M8", "1111");
 BankAccount KamalAccount = new BankAccount(0, "Kamal", 2, "kamal@ranaweera.ca", "780-965-2012", "T6W 0M8", "1111");
+
+
+SupunAccount.Prompt();
+
+
+
+
+
